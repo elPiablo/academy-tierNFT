228623810d8b435a03ea76decf9ea8f82c2874c4
elPiablo <piablo@protonmail.com>
2023-08-01 20:25:28 +0200

    Create new copy of working lesson 3 code. Please check, Markkos

--- tierNFT/scripts/deploy.js
+++ tierNFT/scripts/deploy.js
@@ -1,6 +1,4 @@
-require("ethers");
-// const hre = require('hardhat')
-
+const hre = require('hardhat')
 
 /** Set contract and collection name **/
 const CONTRACT_NAME = 'TierNFT'
@@ -9,18 +7,15 @@ const COLLECTION_SYMBOL = 'Tier'
 
 /** Main deploy function **/
 async function main() {
-  const contractFactory = await ethers.getContractFactory(CONTRACT_NAME)
+  const contractFactory = await hre.ethers.getContractFactory(CONTRACT_NAME)
   const contract = await contractFactory.deploy(
     COLLECTION_NAME,
     COLLECTION_SYMBOL,
   )
-  await contract.waitForDeployment()
-  // original ethers v5:
-  //   console.log(`Contract deployed to ${contract.address}`)
 
-  // Print our newly deployed contract address (ethers v6)
-  const address = await contract.getAddress();
-    console.log(`Contract Address: ${address}`);
+  await contract.waitForDeployment()
+  // Print our newly deployed contract address
+  console.log("Contract deployed at", await contract.getAddress())
 }
 
 /** Run Main function - Do not change **/
